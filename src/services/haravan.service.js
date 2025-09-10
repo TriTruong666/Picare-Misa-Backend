e640daa77fa837c88535fb0653156a466d175968
TriTruong666 <tritruonghoang3@gmail.com>
2025-09-10 18:06:49 +0700

    [UPDATE]

--- src/services/haravan.service.js
+++ src/services/haravan.service.js
@@ -73,7 +73,7 @@ async function fetchAllHaravanOrders(limit = 50) {
 
     // B2: loop qua từng page
     for (let page = 1; page <= totalPages; page++) {
-      console.log(`➡️ Fetch page ${page}/${totalPages}`);
+      console.log(`➡️ Fetch trang ${page}/${totalPages}`);
       const orders = await fetchHaravanOrders(page, limit);
       allOrders = allOrders.concat(orders);
     }
